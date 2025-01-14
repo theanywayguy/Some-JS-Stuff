5731437910fa532ac8a27f25d7bcda821ec7f218
theanywayguy <ajajiphone.am@gmail.com>
2025-01-14 17:50:27 +0300

    new stuff

--- Firstjs/script.js
+++ Firstjs/script.js
@@ -1,11 +1,17 @@
-function hello(){
+function hello(name){
     const head= document.querySelector('h1');
-    if(head.innerHTML==="Hello, World!"){
-       head.innerHTML="Goodbye, World!";
+    if(head.innerHTML===`Hello, ${name}!`){
+       head.innerHTML=`Goodbye, ${name}!`;
     }
     else{
-       head.innerHTML="Hello, World!";
+       head.innerHTML=`Hello, ${name}!`;
     }
  
 
 }
+document.addEventListener('DOMContentLoaded',()=>{document.querySelector('form').onsubmit=()=>{
+   username=document.querySelector('#name').value;
+   
+   hello(username);
+    return false;
+};});
